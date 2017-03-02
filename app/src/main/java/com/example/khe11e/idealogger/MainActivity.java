40dd14f9c3394b3c6d0132510bc538a4946f4245
Kyle Helle <kjhelle4@gmail.com>
2017-03-01 19:09:18 -0800

    Add basic UI

--- app/src/main/java/com/example/khe11e/idealogger/MainActivity.java
+++ app/src/main/java/com/example/khe11e/idealogger/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.khe11e.idealogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ideaText;
+    Button logIdeaBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ideaText = (EditText) findViewById(R.id.ideaText);
+        logIdeaBtn = (Button) findViewById(R.id.logIdeaBtn);
+        
     }
 }
